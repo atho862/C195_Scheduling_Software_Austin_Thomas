0079b79f613ed4aa2ebce5e38099d4f3f5e0835b
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-29 23:02:50 -0500

    Add AppointmentControllers

---
+++ src/Controllers/Appointment/AddAppointmentController.java
@@ -0,0 +1,13 @@
+package Controllers.Appointment;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddAppointmentController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ src/Controllers/Appointment/EditAppointmentController.java
@@ -0,0 +1,13 @@
+package Controllers.Appointment;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditAppointmentController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
