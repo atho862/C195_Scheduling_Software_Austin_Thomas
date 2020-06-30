65044b73723acfc1bdca4a1ff07fa8aa7ca9617c
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-29 23:01:40 -0500

    Update method in interface to throw SQLException

--- src/Contracts/Interfaces/Repositories/IUserRepository.java
+++ src/Contracts/Interfaces/Repositories/IUserRepository.java
@@ -2,9 +2,12 @@ package Contracts.Interfaces.Repositories;
 
 import Infrastructure.Models.User;
 
+import java.sql.SQLException;
+
 public interface IUserRepository {
     void insertUser(User user);
     User getUserById(int userId);
+    User getUserByUsername(String username) throws SQLException;
     void updateUser(User user);
     void deleteUser(int userId);
 }
