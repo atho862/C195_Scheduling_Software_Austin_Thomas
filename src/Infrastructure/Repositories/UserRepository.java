c0c2222c5611664a2d8278a0b98a86a0af761489
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-29 23:05:44 -0500

    Implement getUserByUsername method on UserRepository

--- src/Infrastructure/Repositories/UserRepository.java
+++ src/Infrastructure/Repositories/UserRepository.java
@@ -3,6 +3,10 @@ package Infrastructure.Repositories;
 import Contracts.Interfaces.Repositories.IUserRepository;
 import Infrastructure.DatabaseConnection;
 import Infrastructure.Models.User;
+import com.mysql.jdbc.Statement;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserRepository implements IUserRepository {
 
@@ -14,6 +18,28 @@ public class UserRepository implements IUserRepository {
     @Override
     public User getUserById(int userId) {
         return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) throws SQLException {
+        String userName = new String();
+        String password = new String();
+        int id = 0;
+        Statement statement = (Statement) DatabaseConnection.dbConnection.createStatement();
+        String sql = "SELECT * FROM user WHERE userName = '" + username + "'";
+        ResultSet results = statement.executeQuery(sql);
+        while (results.next()) {
+            userName = results.getString("userName");
+            password = results.getString("password");
+            id = results.getInt("userId");
+        }
+        if (userName != null && password != null && id != 0) {
+            User user = new User(userName, password, id);
+            return user;
+        }
+        else {
+            return null;
+        }
     }
 
     @Override
