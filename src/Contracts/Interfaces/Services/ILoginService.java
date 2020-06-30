aab42efa2ae70c8e4af948c16c3a3ae5755305f2
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-29 23:02:06 -0500

    Add interface for login service

---
+++ src/Contracts/Interfaces/Services/ILoginService.java
@@ -0,0 +1,11 @@
+package Contracts.Interfaces.Services;
+
+import Contracts.Daos.LoginDao;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public interface ILoginService {
+    boolean login(LoginDao loginDao) throws SQLException;
+    void logout(ActionEvent actionEvent) throws IOException;
+}
