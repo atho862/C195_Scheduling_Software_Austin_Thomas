f44256dd40e72048c20181be632fdea77cb81f55
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-29 23:03:00 -0500

    Add Customer Controllers

---
+++ src/Controllers/Customer/CustomerListController.java
@@ -0,0 +1,15 @@
+package Controllers.Customer;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerListController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
