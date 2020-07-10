dfbf954e7a57a46e4448fe6f2d6464210bab7085
atho862 <54923852+atho862@users.noreply.github.com>
2020-07-09 23:19:58 -0500

    Add navigationHelper to handle navigation and remove duplicate code

---
+++ src/Domain/Helpers/NavigationHelper.java
@@ -0,0 +1,29 @@
+package Domain.Helpers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class NavigationHelper {
+
+    Stage stage;
+    Parent root;
+
+    public static void NavigateToLoginScreen(ActionEvent event){
+
+    }
+
+    public static void NavigateToMainScreen(ActionEvent event){
+
+    }
+
+    public static void NavigateToAppointmentListScreen(ActionEvent event){
+
+    }
+
+    public static void NavigateToAddAppointmentScreen(ActionEvent event){
+
+    }
+
+
+}
